6fef3c1df3a56b797e75911fc91fd2d96f4aeaf9
SpyKo_ <111909438+Spykoninho@users.noreply.github.com>
2025-10-30 10:01:38 +0100

    chance test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const ROLLS = [
-    [1, 2, 3, 4, 5, 6],
-]
 export function analyzeDiceRolls(rolls) {
-    
+    let response = 0;
+    for(let i = 0; i<rolls.length; i++){
+        for(let j = 0; j<5; j++){
+            response+=rolls[i][j];
+        }
+    }
+    return response;
 }
--- index.test.js
+++ index.test.js
@@ -1,7 +1,16 @@
 import { describe, expect, it } from "vitest";
+import { analyzeDiceRolls } from ".";
 
 describe("Sample test", ()=>{
     it("Should pass", ()=>{
         expect(true).toBe(true);
+    })
+})
+
+describe("Lucky roll", ()=>{
+    it("Should return 17", ()=>{
+        expect(analyzeDiceRolls([
+            [1, 2, 3, 5, 6]
+        ])).toBe(17)
     })
 })
